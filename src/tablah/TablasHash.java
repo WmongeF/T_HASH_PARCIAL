d669b2abfe4b558bd3c8f46d60a15fe64e05a91b
WmongeF <waltermongee81@gmail.com>
2022-05-05 20:15:18 -0600

    V0.01

---
+++ src/tablah/TablasHash.java
@@ -0,0 +1,17 @@
+
+package tablah;
+import Vista.*;
+
+public class TablasHash {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_hash Hash = new frm_hash();
+        Hash.show();
+        
+    }
+    
+}
